9a1696eecbb56daabadac58e2002263cc991474d
yuukinakamura0925 <ynakamara04@gmail.com>
2022-09-14 05:08:48 +0900

    ルーティング[ホスト名による制約]の設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,25 @@
 Rails.application.routes.draw do
-  namespace :staff, path: "" do
-    # namespaceがついているので、頭にそれぞれstaffがつくイメージ
-    root "top#index"
-    get "login" => "sessions#new", as: :login
-    resource :session, only: [:create, :destroy]
-    resource :account, except: [:new, :create, :destroy]
+  #  baukis2.rbの設定内容
+  config = Rails.application.config.baukis2
+  
+  constraints host: config[:staff][:host] do
+    namespace :staff, path: config[:staff][:path] do
+      root "top#index"
+      get "login" => "sessions#new", as: :login
+      resource :session, only: [ :create, :destroy ]
+      resource :account, except: [ :new, :create, :destroy ]
+    end
   end
 
-  namespace :admin do 
-    root "top#index"
-    get "login" => "sessions#new", as: :login
-    resource :session, only: [:create, :destroy]
-    resources :staff_members
+  constraints host: config[:admin][:host] do
+    namespace :admin, path: config[:admin][:path] do 
+      root "top#index"
+      get "login" => "sessions#new", as: :login
+      resource :session, only: [:create, :destroy]
+      resources :staff_members
+    end
   end
+  
 
   namespace :customer do 
     root "top#index"
