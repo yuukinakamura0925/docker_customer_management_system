3a027065d725f4a5025074417a5192fbf7d28661
yuukinakamura0925 <ynakamara04@gmail.com>
2022-10-04 04:17:28 +0900

    12-2-1 矯正ログアウト

--- app/controllers/staff/base.rb
+++ app/controllers/staff/base.rb
@@ -1,7 +1,8 @@
 class Staff::Base < ApplicationController
   before_action :authorize
+  before_action :check_account
+
    private 
-   
    def current_staff_member
     
     if session[:staff_member_id]
@@ -17,6 +18,15 @@ class Staff::Base < ApplicationController
     unless current_staff_member
       flash.alert = "職員としてログインしてください"
       redirect_to :staff_login
+    end
+  end
+
+  def check_account
+    # ログインしている かつ active?メソッドがfalseだったら
+    if current_staff_member && !current_staff_member.active?
+      session.delete(:staff_member_id)
+      flash.alert = "アカウントが無効になりました"
+      redirect_to :staff_root
     end
   end
 end
