0b32e1da8bcfe46ecf5317c1b63674b5c9e8c1b7
yuukinakamura0925 <ynakamara04@gmail.com>
2022-10-11 03:11:14 +0900

    adminの管理者一覧にページネーション

--- app/controllers/admin/staff_members_controller.rb
+++ app/controllers/admin/staff_members_controller.rb
@@ -2,7 +2,7 @@ class Admin::StaffMembersController < Admin::Base
 
   def index
     # ふりがなを:family_name_kana（姓）, :given_name_kana（名）の順でソートして全て取得
-    @staff_members = StaffMember.order(:family_name_kana, :given_name_kana)
+    @staff_members = StaffMember.order(:family_name_kana, :given_name_kana).page(params[:page])
   end
 
   def show
