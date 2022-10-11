dc1305a6921074f592ba3c78a47bd521237c50df
yuukinakamura0925 <ynakamara04@gmail.com>
2022-10-12 02:32:23 +0900

    14-1-3　氏名とふりがなの正規化とバリデーション

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -1,5 +1,22 @@
 class StaffMember < ApplicationRecord
+include StringNormalizer
+
   has_many :events, class_name: "StaffEvent", dependent: :destroy
+
+  #バリデーションの直前に実行されるコールバック（正規化によってひらがな入力をカタカナに変換する処理している）
+  before_validation do
+    self.family_name = normalize_as_name(family_name)
+    self.given_name = normalize_as_name(given_name)
+    self.family_name_kana = normalize_as_furigana(family_name_kana)
+    self.given_name_kana = normalize_as_furigana(given_name_kana)
+  end
+
+  # 1個以上のカタカナ文字列にマッチする正規表現。\p{katakana}は任意のカタカナ1文字にマッチ。\u{30fc}は長音符1文字にマッチ
+  KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]+\z/
+
+  validates :family_name, :given_name, presence: true
+  validates :family_name_kana, :given_name_kana, presence: true,
+    format: { with: KATAKANA_REGEXP, allow_blank: true }
 
   def password=(raw_password)
     if raw_password.kind_of?(String)
